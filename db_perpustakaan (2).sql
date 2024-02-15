bf24c025abf41f5698ce86481628904b07112ce9
Muhammad Fahry Arief Billah <100301880+FahryArief@users.noreply.github.com>
2024-02-15 14:14:50 +0700

    DATABASE

---
+++ db_perpustakaan (2).sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 15 Feb 2024 pada 05.05
+-- Versi server: 10.1.32-MariaDB
+-- Versi PHP: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` varchar(10) NOT NULL,
+  `id_pengarang` int(11) NOT NULL,
+  `id_penerbit` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `judul_buku` varchar(50) NOT NULL,
+  `tahun_terbit` char(4) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `ebook` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `id_pengarang`, `id_penerbit`, `id_kategori`, `judul_buku`, `tahun_terbit`, `jumlah`, `ebook`) VALUES
+('BK001', 2, 1, 1, 'Kumpulan Cerita Dongeng Anak Anak', '2000', 1, 'Kumpulan_Cerita_Dongeng_Anak_2.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_pm` varchar(20) NOT NULL,
+  `id` int(10) NOT NULL,
+  `id_buku` varchar(10) NOT NULL,
+  `tgl_pinjam` varchar(20) NOT NULL,
+  `tgl_kembali` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Trigger `peminjaman`
+--
+DELIMITER $$
+CREATE TRIGGER `jml_after_pinjam` AFTER INSERT ON `peminjaman` FOR EACH ROW UPDATE buku SET buku.jumlah = buku.jumlah -1 WHERE buku.id_buku = new.id_buku
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penerbit`
+--
+
+CREATE TABLE `penerbit` (
+  `id_penerbit` int(11) NOT NULL,
+  `nama_penerbit` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `penerbit`
+--
+
+INSERT INTO `penerbit` (`id_penerbit`, `nama_penerbit`) VALUES
+(1, 'PPLG');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengarang`
+--
+
+CREATE TABLE `pengarang` (
+  `id_pengarang` int(11) NOT NULL,
+  `nama_pengarang` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pengarang`
+--
+
+INSERT INTO `pengarang` (`id_pengarang`, `nama_pengarang`) VALUES
+(2, 'Fahry Arief');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengembalian`
+--
+
+CREATE TABLE `pengembalian` (
+  `id_pengembalian` int(11) NOT NULL,
+  `id` int(10) NOT NULL,
+  `id_buku` varchar(20) NOT NULL,
+  `tgl_pinjam` char(20) NOT NULL,
+  `tgl_kembali` char(20) NOT NULL,
+  `tgl_dikembalikan` char(20) NOT NULL,
+  `denda` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pengembalian`
+--
+
+INSERT INTO `pengembalian` (`id_pengembalian`, `id`, `id_buku`, `tgl_pinjam`, `tgl_kembali`, `tgl_dikembalikan`, `denda`) VALUES
+(1, 5, 'BK004', '12-02-2024', '19-02-2024', '12-02-2024', '1000'),
+(2, 5, 'BK002', '12-02-2024', '19-02-2024', '12-02-2024', '2000'),
+(3, 5, 'BK002', '12-02-2024', '19-02-2024', '12-02-2024', '0'),
+(4, 5, 'BK002', '12-02-2024', '19-02-2024', '12-02-2024', '1000'),
+(5, 5, 'BK002', '12-02-2024', '19-02-2024', '12-02-2024', '0'),
+(6, 5, 'BK002', '12-02-2024', '19-02-2024', '12-02-2024', '0'),
+(7, 5, 'BK003', '12-02-2024', '19-02-2024', '12-02-2024', '0'),
+(8, 5, 'BK003', '12-02-2024', '19-02-2024', '12-02-2024', '0'),
+(9, 5, 'BK001', '12-02-2024', '19-02-2024', '12-02-2024', '0'),
+(10, 5, 'BK001', '12-02-2024', '19-02-2024', '12-02-2024', '0'),
+(11, 5, 'BK001', '12-02-2024', '19-02-2024', '15-02-2024', '0');
+
+--
+-- Trigger `pengembalian`
+--
+DELIMITER $$
+CREATE TRIGGER `kembali` AFTER INSERT ON `pengembalian` FOR EACH ROW UPDATE buku SET buku.jumlah = buku.jumlah +1 WHERE buku.id_buku = new.id_buku
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_pm`);
+
+--
+-- Indeks untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  ADD PRIMARY KEY (`id_penerbit`);
+
+--
+-- Indeks untuk tabel `pengarang`
+--
+ALTER TABLE `pengarang`
+  ADD PRIMARY KEY (`id_pengarang`);
+
+--
+-- Indeks untuk tabel `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD PRIMARY KEY (`id_pengembalian`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `penerbit`
+--
+ALTER TABLE `penerbit`
+  MODIFY `id_penerbit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengarang`
+--
+ALTER TABLE `pengarang`
+  MODIFY `id_pengarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
